3b6005fe81274cfe81ddb34e4a1878d6931533b8
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-18 17:16:30 +0800

    Update nhblowjob.js

--- plugins/nhblowjob.js
+++ plugins/nhblowjob.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, 'https://h4ck3rs404-api.herokuapp.com/api/nsfw/blowjob?apikey=404Api', '', 'caption', m)
 }
 handler.help = ['blowjob]
-handler.tags = ['Premium']
+handler.tags = ['NSFW']
 handler.command = /^(blowjob)$/i
 
 handler.limit = true

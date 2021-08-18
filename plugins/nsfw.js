c1354f4c6536e6c8c71952faaf282a66ae715607
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-18 17:15:52 +0800

    Update nsfw.js

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, 'https://h4ck3rs404-api.herokuapp.com/api/nsfw/school?apikey=404Api', '', 'caption', m)
 }
 handler.help = ['school]
-handler.tags = ['Premium']
+handler.tags = ['NSFW']
 handler.command = /^(school)$/i
 
 handler.limit = true

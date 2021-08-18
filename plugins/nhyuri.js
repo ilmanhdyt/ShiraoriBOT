308f699d8dd7bcb0b13b2db341b69726ee852538
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-18 17:17:15 +0800

    Update nhyuri.js

--- plugins/nhyuri.js
+++ plugins/nhyuri.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, 'https://h4ck3rs404-api.herokuapp.com/api/nsfw/yuri?apikey=404Api', '', 'caption', m)
 }
 handler.help = ['yuri]
-handler.tags = ['Premium']
+handler.tags = ['NSFW']
 handler.command = /^(yuri)$/i
 
 handler.limit = true
 handler.premium = true
-handler.grup = true
+
 
 module.exports = handler
 

84330a949cc68704c185b28e94ad21513c7c520d
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-20 15:18:11 +0800

    Add files via upload

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -1,14 +1,36 @@
+let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	
-if (args.length == 0) return reply(`Example: ${prefix + command} Gotoubun No Hanayome`)
-  conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/nhentaisearch?apikey=${apikey}&query=${query}`)', '', 'caption', m)
+  let res = await fetch('https://api.lolhuman.xyz/api/nhentai/344253?apikey=39f938655e624cb72a79560b')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
 }
-handler.help = ['school]
+handler.help = ['nhentai']
 handler.tags = ['NSFW']
-handler.command = /^(school)$/i
+handler.command = /^(nhentai)$/i
 
 handler.limit = true
-handler.premium = true
-handler.group = true
 
 module.exports = handler
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://api.lolhuman.xyz/api/nhentai/344253?apikey=39f938655e624cb72a79560b

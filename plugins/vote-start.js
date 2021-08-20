c96b0f09d984f5faacc0af2f4b7dc93392160a90
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-20 22:30:22 +0800

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
-        await conn.sendButton(m.chat, '_Masih ada vote di chat ini!_', '© stikerin', 'HAPUS VOTE', `${usedPrefix}hapusvote`)
+        await conn.sendButton(m.chat, '_Masih ada vote di chat ini!_', '© shiraori', 'HAPUS VOTE', `${usedPrefix}hapusvote`)
         throw false
     }
     await conn.send2Button(m.chat, `Vote dimulai!
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 *${usedPrefix}upvote* - untuk ya
 *${usedPrefix}devote* - untuk tidak
 *${usedPrefix}cekvote* - untuk mengecek vote
-*${usedPrefix}hapusvote* - untuk menghapus vote`, '© stikerin', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`)
+*${usedPrefix}hapusvote* - untuk menghapus vote`, '© shiraori', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`)
     conn.vote[id] = [
         text,
         [],

b186a549e7fb8d9ea779b60759c06683605032f0
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-24 18:59:05 +0800

    Hehe

--- plugins/blowjob.js
+++ plugins/blowjob.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
 
   conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/blowjob?APIKEY=4a8b9aba75823076', '', 'caption', m)
 }
-handler.help = ['blowjob]
+handler.help = ['cum']
 handler.tags = ['NSFW']
-handler.command = /^(blowjob)$/i
+handler.command = /^(cum)$/i
 
 handler.limit = true
 handler.group = true

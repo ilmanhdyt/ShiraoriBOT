caa6838128292bddcfe17cdd645eeee9d96db0e0
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-25 17:31:13 +0800

    Hmm

--- plugins/megumin.js
+++ plugins/megumin.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.url) throw 'Eror!'
-  conn.sendFile(m.chat, json.url, '', '© stikerin', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  conn.sendFile(m.chat, json.url, '', '© shiraori', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
 }
 handler.help = ['megumin']
 handler.tags = ['internet']

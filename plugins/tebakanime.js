0342543d0b9764f7b84b79f8a59a7733668537ea
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-26 10:06:12 +0800

    Anime

---
+++ plugins/tebakanime.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+  conn.tebakanime = conn.tebakanime ? conn.tebakanime : {}
+  let id = m.chat
+  if (id in conn.tebakanime) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakanime[id][0])
+    throw false
+  }
+  let tebakanime = JSON.parse(fs.readFileSync(`./src/tebakanime.json`))
+  let json = tebakanime[Math.floor(Math.random() * tebakanime.length)]
+  let caption = `
+  ${json.deskripsi}
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}hint untuk bantuan
+Bonus: ${poin} XP
+    `.trim()
+  conn.tebakanime[id] = [
+    await conn.sendButtonImg(m.chat, caption, await (await fetch(json.img)).buffer(), '© shiraori', 'BANTUAN', '.hint')
+    ,
+    json, poin,
+    setTimeout(async () => {
+      if (conn.tebakgambar[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© shiraori', 'TEBAK ANIME', '.tebakanime')
+      delete conn.tebakanime[id]
+    }, timeout)
+  ]
+}
+handler.help = ['tebakanime']
+handler.tags = ['game']
+handler.command = /^tebakanime/i
+
+module.exports = handler

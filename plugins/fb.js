be91fbaae79ff1c901a4f216abdac50e7280f012
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-27 21:43:00 +0800

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     // m.reply(require('util').format(json))
     if (!json.status) throw json
     await m.reply(global.wait)
-    await conn.sendFile(m.chat, json.data[0].url, '', '© stikerin', m)
+    await conn.sendFile(m.chat, json.data[0].url, '', '© shiraori', m)
   }).catch(_ => _)
 }
 handler.help = ['fb'].map(v => v + ' <url>')

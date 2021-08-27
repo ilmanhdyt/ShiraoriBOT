2e851ee2600e721b2d85bb2e0a26611c27773eae
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-27 13:19:18 +0800

    Create okickall.js

---
+++ plugins/okickall.js
@@ -0,0 +1,35 @@
+let handler = async (m, { conn, args }) => {
+
+  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+
+  if (!m.mentionedJid[0]) throw `siap dikickall`
+
+  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+
+  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
+
+}
+
+handler.help = ['kickall', '-'].map(v => 'o' + v + ' @member')
+
+handler.tags = ['owner']
+
+handler.command = /^(okickall|o\-)$/i
+
+handler.owner = true
+
+handler.mods = false
+
+handler.premium = false
+
+handler.group = true
+
+handler.private = false
+
+handler.admin = false
+
+handler.botAdmin = true
+
+handler.fail = null
+
+module.exports = handler

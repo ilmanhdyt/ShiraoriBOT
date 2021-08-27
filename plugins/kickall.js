491bb130b5b5c59d6146d55107d46be0ffcab38f
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-28 00:51:13 +0800

    Merapikan

--- plugins/kickall.js
+++ plugins/kickall.js
@@ -1,15 +1,15 @@
 case 'kickall':
-		if (!isOwner) return reply(ind.ownerb())
-		members_id = []
-		teks = (args.length > 1) ? body.slice(8).trim() : ''
-		teks += '\n\n'
-		for (let mem of groupMembers) {
-		    teks += `*sayonara* ${mem.jid.split('@')[0]}\n`
-		    members_id.push(mem.jid)
-	   }
-	  mentions(teks, members_id, true)
-      client.groupRemove(from, members_id)
-	  break
+					if (!isOwner) return reply(ind.ownerb())
+					members_id = []
+					teks = (args.length > 1) ? body.slice(8).trim() : ''
+					teks += '\n\n'
+					for (let mem of groupMembers) {
+						teks += `*sayonara* ${mem.jid.split('@')[0]}\n`
+						members_id.push(mem.jid)
+					}
+					mentions(teks, members_id, true)
+					client.groupRemove(from, members_id)
+					break
 }
 handler.help = ['kickall']
 handler.tags = ['admin']
@@ -27,4 +27,5 @@ handler.fail = null
 handler.limit = true
 
 module.exports = handler
+
 

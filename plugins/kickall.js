5245714086f89a66abda89d882c4da0192cc7243
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-28 12:29:47 +0800

    Kickall

--- plugins/kickall.js
+++ plugins/kickall.js
@@ -5,7 +5,7 @@ teks = (args.length > 1) ? body.slice(8).trim() : ''
 teks += '\n\n'
 for (let mem of groupMembers) {
      teks += `*sayonara* ${mem.jid.split('@')[0]}\n`
-	 members_id.push(mem.jid)
+     members_id.push(mem.jid)	
 }
 mentions(teks, members_id, true)
 client.groupRemove(from, members_id)

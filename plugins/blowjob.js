38a771a80b3d0ca693dbc0745a81fb256952f278
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-28 22:59:21 +0800

    Update blowjob.js

--- plugins/blowjob.js
+++ plugins/blowjob.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
  
   conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/blowjob?APIKEY=4a8b9aba75823076', '', 'sange~an', m)
-  if (json.nsfw && !db.data.settings.nsfw) throw 'Mode NSFW tidak aktif'
+  
 }
 handler.help = ['cum']
 handler.tags = ['NSFW']

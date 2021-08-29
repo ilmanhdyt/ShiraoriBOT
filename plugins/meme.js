55444f9428d173cf85a3321ee1f0a65b747a1991
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 15:01:09 +0800

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,1 +1,12 @@
-https://api.xteam.xyz/randomimage/meme?APIKEY=FuzBot1
+
+
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/meme?APIKEY=FuzBot1', '', 'Garing😑', m)
+}
+handler.help = ['meme']
+handler.tags = ['fun']
+handler.command = /^(meme)$/i
+
+handler.limit = true
+
+module.exports = handler

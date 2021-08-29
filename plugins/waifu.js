a6cbbd5f33181cdb223241c1f65e6229acbe9ba7
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 15:38:44 +0800

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['waifu']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(waifu)$/i
 
 handler.limit = true

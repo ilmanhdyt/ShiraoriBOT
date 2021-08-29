b4f59b7fb02341722d21023eba97c30c3c456cec
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 11:44:24 +0800

    Update ahegao.js

--- plugins/ahegao.js
+++ plugins/ahegao.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/ahegao?APIKEY=4a8b9aba75823076', '', '', m)
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/ahegao?APIKEY=4a8b9aba75823076', '', 'sange?', m)
 }
 handler.help = ['ahegao']
-handler.tags = ['NSFW']
+handler.tags = ['nsfw']
 handler.command = /^(ahegao)$/i
 
 handler.limit = true

678ed3cd9412360af838a5d410f2c32e7ca5e72f
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 15:18:28 +0800

    Update snackvideo.js

--- plugins/snackvideo.js
+++ plugins/snackvideo.js
@@ -2,13 +2,13 @@ let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'uhm.. url nya mana?'
   global.API('xteam', '/dl/', {
     url: args[0]
-  }, 'APIKEY')
-  conn.sendFile(m.chat, undefined, '', '', m)
+  }, 'FuzBot1')
+  conn.sendFile(m.chat, undefined, '', '© Shiraori', m)
 }
 handler.help = ['snackvideo'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^\x00s$/i
+handler.command = /^\snackvideo/i
 handler.limit = true
 
 module.exports = handler

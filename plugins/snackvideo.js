9890224a22c0758ad4de0d5594f7ae2c8144f6af
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 15:10:55 +0800

    Update snackvideo.js

--- plugins/snackvideo.js
+++ plugins/snackvideo.js
@@ -5,10 +5,10 @@ let handler = async (m, { conn, args }) => {
   }, 'APIKEY')
   conn.sendFile(m.chat, undefined, '', '', m)
 }
-handler.help = [].map(v => v + ' <url>')
+handler.help = ['snackvideo'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^\x00s$/i
-handler.disabled = true
+handler.limit = true
 
 module.exports = handler

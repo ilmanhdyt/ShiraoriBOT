47a99eb0fdeaf18b89b7d4836391831201d02736
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 11:30:44 +0800

    Update futanari.js

--- plugins/futanari.js
+++ plugins/futanari.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://hardianto-chan.herokuapp.com/api/anime/random?nsfw=futanari&apikey=hardianto', '', '', m)
+  conn.sendFile(m.chat, 'https://hardianto-chan.herokuapp.com/api/anime/random?nsfw=futanari&apikey=hardianto', '', 'kyaaa', m)
 }
 handler.help = ['futanari']
-handler.tags = ['NSFW']
+handler.tags = ['nsfw']
 handler.command = /^(futanari)$/i
 
 handler.limit = true

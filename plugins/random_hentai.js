8783827b17b48e0b7fc8a898512e471f79add4f0
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 15:53:47 +0800

    Update random_hentai.js

--- plugins/random_hentai.js
+++ plugins/random_hentai.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
   
-  conn.sendFile(m.chat, 'https://pencarikode.xyz/api/hentai?apikey=pais', '', '', m)
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/hentai?APIKEY=FuzBot1', '', 'hentai', m)
 }
-handler.help = ['hent']
-handler.tags = ['NSFW']
-handler.command = /^(hent)$/i
+handler.help = ['hentai']
+handler.tags = ['nsfw']
+handler.command = /^(hentai)$/i
 
 handler.limit = true
 handler.group = true

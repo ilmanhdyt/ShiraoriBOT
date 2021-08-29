5be69e0886de358e24b1ba0be60d6ddcf1c522ba
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-29 11:29:48 +0800

    Update random_neko.js

--- plugins/random_neko.js
+++ plugins/random_neko.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://hardianto-chan.herokuapp.com/api/anime/random?nsfw=neko&apikey=hardianto', '', '', m)
-  if (json.nsfw && !db.data.settings.nsfw) throw 'Mode NSFW tidak aktif'
+  conn.sendFile(m.chat, 'https://hardianto-chan.herokuapp.com/api/anime/random?nsfw=neko&apikey=hardianto', '', 'ahhhh', m)
+  
 }
 handler.help = ['nekoo']
-handler.tags = ['NSFW']
+handler.tags = ['nsfw']
 handler.command = /^(nekoo)$/i
 
 handler.limit = true

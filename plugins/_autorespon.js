202cdef1c315c811f3477d4cc2215d397b5ad262
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-30 23:06:13 +0800

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -28,8 +28,8 @@ handler.all = async function (m, { isBlocked }) {
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
         this.sendButton(m.chat, `┌〔 Undang Bot ke Grup 〕
-├ 7 Hari / Rp 7,000
-├ 30 Hari / Rp 15,000
+├ 7 Hari / Rp 10,000
+├ 30 Hari / Rp 25,000
 ├ Premium / Rp 5,000
 └────
 

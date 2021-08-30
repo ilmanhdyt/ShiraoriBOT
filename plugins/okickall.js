e338cec554840d1ed83610b92e515aaf702635fc
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-30 15:07:08 +0800

    Update okickall.js

--- plugins/okickall.js
+++ plugins/okickall.js
@@ -1,35 +1,28 @@
 let handler = async (m, { conn, args }) => {
-
-  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-
-  if (!m.allmem[0]) throw `siap dikickall`
-
-  let users = m.allmem.filter(u => !(u == ownerGroup || u.includes(conn.allmem.jid)))
-
-  for (let allmem) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [allmem])
-
+         let members_id = groupMembers.map(v => v.jid)
+          let mentioned = members_id
+       let using = mentioned.filter(u => !(u == isOwner || u.includes(conn.user.jid)))
+                for (let member of using) {
+                if (member.endsWith('@s.whatsapp.net')) 
+                await delay(3000)
+                await conn.groupRemove(from, members_id)
+                }
+               await m.reply(m.chat, 'Sukses Kick All Member', m)
 }
-
-handler.help = ['kickall', '-'].map(v => 'o' + v + ' @member')
-
+handler.help = ['okickall']
 handler.tags = ['owner']
-
-handler.command = /^(okickall|o\-)$/i
-
+handler.command = /^(okickall)$/i
+handler.rowner = true
 handler.owner = true
-
 handler.mods = false
-
 handler.premium = false
-
 handler.group = true
-
 handler.private = false
 
-handler.admin = false
-
+handler.admin = true
 handler.botAdmin = true
 
 handler.fail = null
+handler.limit = false
 
 module.exports = handler

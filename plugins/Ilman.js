5b3d77a9df68cf3d7cc262374ba34f321b3d7fcd
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-08-31 20:42:30 +0800

    Update Ilman.js

--- plugins/Ilman.js
+++ plugins/Ilman.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Ilman Onii-Chan Punya  >///<
+Elaina-chan Punya Ilman Onii-chan  >///<
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /ilman/i
+handler.customPrefix = /Elaina/i
 handler.command = new RegExp
 
 module.exports = handler

78367feda61b9e45b12cec20f2814abe96d15a2b
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-01 14:11:14 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -185,7 +185,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       return conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
         "listMessage": {
           "title": `${ucapan()}, ${name}`.trim(),
-          "description": "© SHIRAORI BOT",
+          "description": "© SHIRAORIBOT",
           "buttonText": "Klik Disini",
           "listType": "SINGLE_SELECT",
           "sections": [

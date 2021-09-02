b15b0dbc226e3277a8d5808d2cb5181f7a605ca4
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-02 12:11:59 +0800

    Update 18+.js

--- plugins/18+.js
+++ plugins/18+.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn }) => {
 	
-	await m.reply('[❗] WAIT, Tunggu Sebentar:v.')
+	await m.reply('Sabar Yaa Onii-chan.')
   conn.reply(m.chat,`${pickRandom(global.bokep)}`, m)
 }
 handler.help = ['filebokep']

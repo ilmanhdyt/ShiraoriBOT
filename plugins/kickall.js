214fc9ecc93f19e1a5311fe92f0b45eaa6fe5c1e
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-03 08:40:34 +0800

    Create kickall.js

---
+++ plugins/kickall.js
@@ -0,0 +1,16 @@
+if (!isGroupMsg) return client.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
+            const isGroupOwner = sender.id === chat.groupMetadata.owner
+            if (!isGroupOwner) return client.reply(from, 'Perintah ini hanya bisa di gunakan oleh Owner group', id)
+            if (!isBotGroupAdmins) return client.reply(from, 'Perintah ini hanya bisa di gunakan ketika bot menjadi admin', id)
+            const allMem = await client.getGroupMembers(groupId)
+            for (let i = 0; i < allMem.length; i++) {
+                if (groupAdmins.includes(allMem[i].id)) {
+                    console.log('Upss this is Admin group')
+                } else {
+                    await client.removeParticipant(groupId, allMem[i].id)
+                }
+            }
+            client.reply(from, 'Succes kick all member', id)
+            break
+}
+handler.help

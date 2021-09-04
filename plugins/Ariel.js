c53a617732e9c0aa57263d3aa275412bada4834b
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-04 23:21:28 +0800

    Update Ariel.js

--- plugins/Ariel.js
+++ plugins/Ariel.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Ariel-sama Wangyy Wangyy
+Ariel-sama Kawaii
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /Ariel/i
+handler.customPrefix = /@6288279550172/i
 handler.command = new RegExp
 
 module.exports = handler

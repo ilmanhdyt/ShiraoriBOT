92bf7c8780aae7247cb5ea79e60321e80da44860
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-04 23:19:55 +0800

    Update tagowner.js

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Ilman Onii-Chan ada yg manggil >///<
+Ada Apa Nge Tag Onii-chan Mau Di Kick?
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /Ilman/i
+handler.customPrefix = /@6281351047727/i
 handler.command = new RegExp
 
 module.exports = handler

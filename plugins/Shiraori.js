857dd4b23bbcdb1bd3dd44847c891fed62e0035b
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-08 19:47:06 +0800

    Update Shiraori.js

--- plugins/Shiraori.js
+++ plugins/Shiraori.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Haiiikk Ilman Onii-chan  >///<
+Haiiikk Goshujin-sama
 `.trim(), m)
     let mentionedJid = [m.sender]
 }

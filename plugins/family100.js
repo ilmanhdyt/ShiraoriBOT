1ad9f3ea889e47dc6195f1c7f3f2fdab91c4b224
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-10 07:27:45 +0800

    Update

--- plugins/family100.js
+++ plugins/family100.js
@@ -1,26 +1,24 @@
-let fs = require('fs')
+let fetch = require('node-fetch')
 let winScore = 500
 async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
     if (id in this.game) {
-        this.sendButton(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', '© shiraori', 'NYERAH', 'nyerah', { quoted: this.game[id].msg })
+        this.sendButton(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', '© SHIRAORI', 'Nyerah', 'nyerah', this.game[id].msg)
         throw false
     }
-    let src = JSON.parse(fs.readFileSync(`./src/family.json`))
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/family100.json')).json()
     let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 *Soal:* ${json.soal}
-
 Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
 (beberapa jawaban terdapat spasi)
 `: ''}
-
 +${winScore} XP tiap jawaban benar
     `.trim()
     this.game[id] = {
         id,
-        msg: await this.sendButton(m.chat, caption, '© shiraori', 'NYERAH', 'nyerah'),
+        msg: await this.sendButton(m.chat, caption, '© SHIRAORI', 'Nyerah', 'nyerah', m),
         ...json,
         terjawab: Array.from(json.jawaban, () => false),
         winScore,

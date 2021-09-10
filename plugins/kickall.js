71ab1159d854abf0ae9b20ba50ac65bb5c1e6437
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-10 08:38:02 +0800

    GK tau bisa atau nggak

--- plugins/kickall.js
+++ plugins/kickall.js
@@ -1,32 +1,30 @@
-if (!isGroupMsg) return client.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
-            const isGroupOwner = sender.id === chat.groupMetadata.owner
-            if (!isGroupOwner) return client.reply(from, 'Perintah ini hanya bisa di gunakan oleh Owner group', id)
-            if (!isBotGroupAdmins) return client.reply(from, 'Perintah ini hanya bisa di gunakan ketika bot menjadi admin', id)
-            const allMem = await client.getGroupMembers(groupId)
-            for (let i = 0; i < allMem.length; i++) {
-                if (groupAdmins.includes(allMem[i].id)) {
-                    console.log('Upss this is Admin group')
-                } else {
-                    await client.removeParticipant(groupId, allMem[i].id)
+let handler = async (m, { conn, args }) => {
+         let members_id = groupMembers.map(v => v.jid)
+          let mentioned = members_id
+       let using = mentioned.filter(u => !(u == isOwner || u.includes(conn.user.jid)))
+                for (let member of using) {
+                if (member.endsWith('@s.whatsapp.net')) 
+                await delay(3000)
+                await conn.groupRemove(from, members_id)
                 }
-            }
-            client.reply(from, 'Succes kick all member', id)
-            break
+               await m.reply(m.chat, 'Sukses Kick All Member', m)
 }
 handler.help = ['kickall']
 handler.tags = ['group']
 handler.command = /^(kickall)$/i
-
+handler.rowner = false
 handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = true
-handler.rowner = true
 handler.private = false
 
 handler.admin = true
-handlet.botAdmin = true
+handler.botAdmin = true
 
 handler.fail = null
+handler.limit = false
 
 module.exports = handler
+
+

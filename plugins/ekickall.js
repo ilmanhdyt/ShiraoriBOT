78bcee87501618e28407cc746b261585f763b865
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-13 10:28:51 +0800

    I

--- plugins/ekickall.js
+++ plugins/ekickall.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args }) => {
   let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
   let members = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.members.jid)))
-  for (let member of members) if (member.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [members])
+  for (let member of members) if (members.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, members)
 }
-handler.help = ['kickall']
+handler.help = ['ekickall']
 handler.tags = ['group']
-handler.command = /^(kickall)$/i
+handler.command = /^(ekickall)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false

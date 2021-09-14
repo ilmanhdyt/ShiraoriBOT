52edeec2806aadd97d3c538345a44cf3ee3dc993
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-14 11:13:59 +0800

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -8,8 +8,8 @@ let handler = async function (m, { text, usedPrefix, command }) {
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
   age = parseInt(age)
-  if (age > 70) throw 'Umur terlalu tua'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  if (age > 40) throw 'Umur terlalu tua'
+  if (age < 13) throw 'Bayi bisa ngetik sesuai format bjir ._.'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date

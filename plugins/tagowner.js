9a5b2ef1a51f9a968464f195cff999597f5d369f
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-17 13:49:39 +0800

    Update tagowner.js

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Ada Apa Nge Tag Onii-chan Mau Di Kick?
+? ada apa nge tag?
 `.trim(), m)
     let mentionedJid = [m.sender]
 }

dbb843c2adce6198ec520f4adf218631469969d1
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-17 13:47:33 +0800

    Update Ariel.js

--- plugins/Ariel.js
+++ plugins/Ariel.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Ariel-sama Kawaii
+Njer Ni orang Mau di kick
 `.trim(), m)
     let mentionedJid = [m.sender]
 }

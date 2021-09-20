b150d051eb8493c3391ae45ab43fb29060e8c65c
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-20 08:27:29 +0800

    ButtonImg

--- plugins/megumin.js
+++ plugins/megumin.js
@@ -5,6 +5,7 @@ let handler = async (m, { conn }) => {
   let json = await res.json()
   if (!json.url) throw 'Eror!'
   conn.sendFile(m.chat, json.url, '', '© shiraori', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  await conn.sendButtonImg(m.chat, await (await fetch(json.img)).buffer(), Klik Disni, '© SHIRAORI', 'Next', '.megumin', m)
 }
 handler.help = ['megumin']
 handler.tags = ['anime']

245bd30284819675013b84335065af0329ef4802
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-23 23:09:11 +0800

    Update rkick.js

--- plugins/rkick.js
+++ plugins/rkick.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.qouted ? m.quoted.sender : ''
 await conn.groupRemove(m.chat, [who || m.quoted.sender])
 }
-handler.help = ['rkick' ('reply chat')]
+handler.help = ['rkick']
 handler.tags = ['admin']
 handler.command = /^(rkick)$/i
 

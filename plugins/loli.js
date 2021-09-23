b19622b1d6e01c5b74ed83aad5e368f5a885ce58
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-23 16:46:38 +0800

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
  
   conn.sendFile(m.chat, 'https://hardianto-chan.herokuapp.com/api/anime/loli?apikey=hardianto', '', 'lolicon ajg', m)
-  
+  await conn.sendButtonImg(m.chat, await (await fetch(json.img)).buffer(), caption, '© SHIRAORI', 'Next', '.loli', m)
 }
 handler.help = ['loli']
 handler.tags = ['anime']

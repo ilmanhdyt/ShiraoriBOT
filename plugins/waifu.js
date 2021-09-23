76db74fb8cc3618ba9e21c815c51af80ef9fb856
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-23 22:49:06 +0800

    Create waifu.js

---
+++ plugins/waifu.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/sfw/waifu')
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
+}
+handler.help = ['waifu']
+handler.tags = ['anime']
+handler.command = /^(waifu)$/i
+
+handler.limit = true
+
+module.exports = handler

f3ba037cbe91310c9e588b5c1143e45a91d1ba99
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-24 23:39:20 +0800

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   let json = await res.json()
   // if (!json.status) throw json
   await m.reply(global.wait)
-  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\n© stikerin`, m)
+  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\n© SHIRAORI`, m)
 
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')

5c5db208492b2d22e5999d4232b0dd5769a36793
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-24 23:05:35 +0800

    Create asupanloli.js

---
+++ plugins/asupanloli.js
@@ -0,0 +1,16 @@
+let fetch = require("node-fetch");
+let arr = [];
+fetch("https://raw.githubusercontent.com/AlvioAdjiJanuar/random/main/loli.txt")
+  .then((res) => res.text())
+  .then((txt) => (arr = txt.split("\n")));
+let handler = async (m, { conn }) => {
+  let loli = arr[Math.floor(Math.random() * arr.length)];
+  if (!loli) throw false;
+  await conn.sendFile(m.chat, loli, loli, ":<", m, 0);
+};
+handler.command = /^(asupanloli)?)$/i;
+handler.help = ["asupanloli"];
+handler.tags = ["anime"];
+
+handler.limit = true;
+module.exports = handler;

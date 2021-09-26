00a8f1e2747c037aadf6ea3dc28ee5ed0af377a0
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 11:55:02 +0800

    Update moderator.js

--- plugins/moderator.js
+++ plugins/moderator.js
@@ -1,9 +1,1 @@
-function handler(m) {
-  this.sendContact(m.chat, './src/moderator.json', this.getName('[]@s.whatsapp.net'), m)
-}
-handler.help = ['mods', 'moderator']
-handler.tags = ['info']
-
-handler.command = /^(mods|moderator)$/i
-
-module.exports = handler
+q

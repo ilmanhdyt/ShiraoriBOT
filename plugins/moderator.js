65057b0f17795f5683e3a7236758004123e4229c
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 12:04:16 +0800

    Update moderator.js

--- plugins/moderator.js
+++ plugins/moderator.js
@@ -1,10 +1,2 @@
 let fs = require('fs')
-let handler = async (m, { conn, isOwner }) => {
-  let mods = global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-}
-handler.help = ['moderator|mods']
-handler.tags = ['owner']
-handler.command = /^(moderator|mods)$/i
-
-module.exports = handler
 

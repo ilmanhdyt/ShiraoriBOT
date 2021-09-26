470a7c1a787fbceb52271fe3c123748ad4a4d977
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 11:22:59 +0800

    Update moderator.js

--- plugins/moderator.js
+++ plugins/moderator.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  this.sendContact(m.chat, '62895803135347', this.getName('62895803135347@s.whatsapp.net'), m)
+  
 }
 handler.help = ['mods', 'moderator']
 handler.tags = ['info']

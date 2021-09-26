cae6ed14d6fdcf29b8aff55dd92ea098988eed46
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 12:51:43 +0800

    Update modslist.js

--- plugins/modslist.js
+++ plugins/modslist.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, isOwner }) => {
   let mods = global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
   conn.reply(m.chat, `┌〔 Daftar Moderator 〕` + `\n` + mods.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: mods } })
 }
-handler.help = ['modslist']
+handler.help = ['moderator']
 handler.tags = ['info']
-handler.command = /^(mods|moderator)$/i
+handler.command = /^(moderator)$/i
 
 module.exports = handler

1903d112e3bd03b528ff9201478fa029564c8010
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 12:16:40 +0800

    Delete moderator.js

--- plugins/moderator.js
+++
@@ -1,8 +0,0 @@
-let fs = require('fs')
-let mods = global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-}
-handler.help = ['moderator']
-handler.tags = ['owner']
-handler.command = /^moderator$/i
-
-module.exports = handler

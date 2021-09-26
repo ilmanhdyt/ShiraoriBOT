0a0a88b78df21d9718d0457daf3098e79b37b2b4
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 10:56:20 +0800

    Update mkick.js

--- plugins/mkick.js
+++ plugins/mkick.js
@@ -3,7 +3,7 @@ let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.qouted ? m.quoted.sender : '
 await conn.groupRemove(m.chat, [who || m.quoted.sender])
 }
 handler.help = ['mkick']
-handler.tags = ['moderator']
+handler.tags = ['mods']
 handler.command = /^(mkick)$/i
 
 handler.group = true

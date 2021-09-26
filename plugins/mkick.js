507441355c419c79a240140812ad6650b2e662ff
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 10:25:45 +0800

    Create mkick.js

---
+++ plugins/mkick.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn }) => {
+let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.qouted ? m.quoted.sender : ''
+await conn.groupRemove(m.chat, [who || m.quoted.sender])
+}
+handler.help = ['mkick']
+handler.tags = ['moderator']
+handler.command = /^(mkick)$/i
+
+handler.group = true
+handler.private = false
+handler.premium = false
+
+handler.mods = true
+handler.botAdmin = true
+
+handler.fail = null
+
+module.exports = handler

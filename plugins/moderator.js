52a7397b7110a17716538ab8c81450749ff0e47a
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 11:57:02 +0800

    Update moderator.js

--- plugins/moderator.js
+++ plugins/moderator.js
@@ -1,1 +1,5 @@
-q
+let fs = require('fs')
+let handler = async (m, { conn, isOwner }) => {
+  let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
+  
+

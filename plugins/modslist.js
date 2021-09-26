294323a098e43e05e4d9f27b85b6d282b355dc7a
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-26 08:37:23 +0800

    Create modslist.js

---
+++ plugins/modslist.js
@@ -0,0 +1,10 @@
+let fs = require('fs')
+let handler = async (m, { conn, isOwner }) => {
+  let mods = global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
+  conn.reply(m.chat, `┌〔 Daftar Moderator 〕` + `\n` + mods.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: mods } })
+}
+handler.help = ['modslist']
+handler.tags = ['owner']
+handler.command = /^(listmods|modslist)$/i
+
+module.exports = handler

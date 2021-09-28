0ee48a0509b45f92badc6b0afd1ee6dd1f4bc416
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-28 09:34:05 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -140,7 +140,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
   if (!args[0]) {
     conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
       "listMessage": {
-        "title": `${ucapan}, ${name}
+        "title": `${ucapan}, ${name},
         "description": "Pilih Dibawah ya...",
         "buttonText": "Klik Disini",
         "listType": "SINGLE_SELECT",

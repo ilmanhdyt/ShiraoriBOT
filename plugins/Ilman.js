60ea19c236d303f491a1e66333d20474cb5f5ad2
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-30 00:52:39 +0800

    Update Ilman.js

--- plugins/Ilman.js
+++ plugins/Ilman.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-https://github.com/Hidayatilman/ShiraoriBOT
+https://github.com/Hidayatilman/Keqing
 `.trim(), m)
     let mentionedJid = [m.sender]
 }

453f60da3b67ab2611dd6d0e3aba3afbcf019b22
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-09-30 09:04:36 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -290,7 +290,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
                   "description": "",
                   "rowId": ".? nsfw"
                 }, {
-                  "title": "Anime"
+                  "title": "Anime",
                   "description": "",
                   "rowId": ".? anime"
                 }, {

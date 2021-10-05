4dbd9d6cde4e98d9401b01261a7e7424897774c6
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-05 14:53:41 +0800

    Delete creator.js

--- plugins/creator.js
+++
@@ -1,9 +0,0 @@
-function handler(m) {
-  this.sendContact(m.chat, '6281351047727', this.getName('6281351047727@s.whatsapp.net'), m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler

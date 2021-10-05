167adbab05e0072aedd489376697d08af7f51777
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-05 15:09:07 +0800

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  await conn.sendButtonImg(m.chat, await (await fetch(json.img)).buffer(), caption, '© SHIRAORI', 'Next', '.waifu', m)
+  await conn.sendButtonLoc(m.chat, await (await fetch(json.img)).buffer(), caption, '© SHIRAORI', 'Next', '.waifu', m)
 }
 handler.help = ['waifu']
 handler.tags = ['anime']

a7f369e896c22aec27ef8ad80af4be362fa474f0
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-06 19:38:11 +0800

    Create nekopoi.js

---
+++ plugins/nekopoi.js
@@ -0,0 +1,17 @@
+let { getVideo } = require('../lib/nekopoi')
+let handler = async (m, { text }) => {
+ if (!text) throw 'Tidak Ada Url'
+ let { title, links } = await getVideo(text)
+ if (links.length == 0) throw 'Video Tidak Ditemukan!'
+ teksnya = `*[NEKOPOI DOWNLOADER]*\n\nTitle : ${title}\n\n${links.join('\n')}`
+ m.reply(teksnya.trim())
+}
+handler.help = ['nekopoi'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^nekopoi$/i
+
+handler.nsfw = true
+handler.group = true
+handler.limit = true
+
+module.exports = handler

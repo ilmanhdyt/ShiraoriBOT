f8c6c9fd2fc518eaac0a929bc8e1387fa4dfab0e
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-06 09:55:59 +0800

    Create nhentai.js

---
+++ plugins/nhentai.js
@@ -0,0 +1,26 @@
+const { WAConnection: _WAConnection, MessageType, Mimetype } = require('@adiwajshing/baileys')
+let axios = require('axios');
+let handler = async (m, { conn, usedPrefix: _p, text }) => {
+	if (!text) throw `Masukan kodenya\nContoh : ${_p}nhentai 375518`
+	axios.get(`https://kelliotnhentai.herokuapp.com/?id=${text}`).then(res => {
+    	if(res.data.status) throw res.data.msg
+    	conn.sendMessage(m.chat, res.data, MessageType.document, { mimetype: Mimetype.pdf, filename: `nhentai.pdf` })
+	})
+}
+handler.help = ['nhentai']
+handler.tags = ['downloader']
+handler.command = /^nhentai/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
+
+module.exports = handler

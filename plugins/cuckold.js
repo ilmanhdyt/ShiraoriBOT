553d0d969d85d15330994c9c11aa1703ce11a18f
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-07 20:40:31 +0800

    Update cuckold.js

--- plugins/cuckold.js
+++ plugins/cuckold.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/cuckold?APIKEY=c81b3345e477a0c7', '', 'Ter-NTR', m)
 }
-handler.help = ['ntr']
+handler.help = ['cuckold']
 handler.tags = ['nsfw']
-handler.command = /^(ntr)$/i
+handler.command = /^(cuckold)$/i
 
 handler.limit = true
 handler.group = true
+handler.nsfw = true
 
 module.exports = handler

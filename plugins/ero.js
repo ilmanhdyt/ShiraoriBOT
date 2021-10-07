f6a4cac1fe939b24c734c2930727803d7491d181
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-07 17:47:29 +0800

    Update ero.js

--- plugins/ero.js
+++ plugins/ero.js
@@ -1,10 +1,11 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/ero?APIKEY=FuzBot1', '', '', m)
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/ero?APIKEY=FuzBot1', '', 'ahhh', m)
 }
 handler.help = ['ero']
 handler.tags = ['nsfw']
 handler.command = /^(ero)$/i
 
 handler.limit = true
+handler.nsfw = true
 
 module.exports = handler

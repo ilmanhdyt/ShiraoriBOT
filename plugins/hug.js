4265c01664a0ec468e567b61c571b1468620c457
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-08 10:46:28 +0800

    Create hug.js

---
+++ plugins/hug.js
@@ -0,0 +1,33 @@
+let fetch = require("node-fetch");
+let handler = async (m, { conn, command }) => {
+	if (m.quoted && m.quoted.sender) m.mentionedJid.push(m.quoted.sender);
+	if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
+	let res = await fetch(
+		API("https://some-random-api.ml", "/animu/" + command, {})
+	);
+	if (!res.ok) throw `${res.status} ${res.statusText}`;
+	let json = await res.json();
+	if (json.link)
+		conn.sendFile(
+			m.chat,
+			json.link,
+			"wibukontol.gif",
+			`@${m.sender.split("@")[0]} ${command} ${m.mentionedJid
+				.map((user) =>
+					user === m.sender ? "themselves " : `@${user.split("@")[0]}`
+				)
+				.join(", ")}`,
+			m,
+			false,
+			{
+				asGIF: true,
+				contextInfo: {
+					mentionedJid: [...m.mentionedJid, m.sender],
+				},
+			}
+		);
+	else throw json;
+};
+handler.help = ["hug", "pat", "wink"];
+handler.tags = ["anime"];
+handler.command = /^(hug|pat|wink)$/i;

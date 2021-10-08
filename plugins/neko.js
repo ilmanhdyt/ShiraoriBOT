925c1f823524019e3133263b1d2bb6c957822dd1
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-08 10:45:37 +0800

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.url) throw 'Eror!'
-  conn.sendFile(m.chat, json.url, '', '© shiraori', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  conn.sendFile(m.chat, json.url, '', 'nyaa', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
 }
 handler.help = ['neko']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^neko$/i
 
 handler.limit = true

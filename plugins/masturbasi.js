2a004d1c20a750a0a8f4b7ce9b0f17b15f86ce18
Ilman <87686497+Hidayatilman@users.noreply.github.com>
2021-10-08 20:59:20 +0800

    Update masturbasi.js

--- plugins/masturbasi.js
+++ plugins/masturbasi.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/mstrb?APIKEY=4a8b9aba75823076', '', '', m)
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/mstrb?APIKEY=4a8b9aba75823076', '', 'sange kok sama kartun', m)
 }
 handler.help = ['horny']
-handler.tags = ['NSFW']
+handler.tags = ['nsfw']
 handler.command = /^(horny)$/i
 
 handler.limit = true
 handler.group = true
+handler.nsfw = true
 
 module.exports = handler

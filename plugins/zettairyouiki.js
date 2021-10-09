30769306945da84b4490ce243f2127b8d435de61
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-09 16:29:55 +0800

    Update zettairyouiki.js

--- plugins/zettairyouiki.js
+++ plugins/zettairyouiki.js
@@ -1,10 +1,11 @@
 let handler = async (m, { conn }) => {
+  
   conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/zettairyouiki?APIKEY=FuzBot1', '', '© Shiraori', m)
 }
-handler.help = ['zettai']
-handler.tags = ['premium']
-handler.command = /^(zettai)$/i
+handler.help = ["zettaityouiki"]
+handler.tags = ["anime"]
+handler.command = /^(zettairyouiki)$/i
 
-handler.premium = true
+handler.limit = true
 
 module.exports = handler

0609794072b802429c90f76ed0d4cb7b85e7b1a6
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-09 17:29:32 +0800

    Update panties.js

--- plugins/panties.js
+++ plugins/panties.js
@@ -1,13 +1,14 @@
 let handler = async (m, { conn }) => {
  
-  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/panties?APIKEY=HIRO', '', 'sange~an', m)
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/panties?APIKEY=HIRO', '', 'aku melihat pantsu', m)
   
 }
-handler.help = ['panties']
+handler.help = ['pantsu']
 handler.tags = ['nsfw']
-handler.command = /^(panties)$/i
+handler.command = /^(pantsu)$/i
 
 handler.limit = true
 handler.group = true
+handler.nsfw = true
 
 module.exports = handler

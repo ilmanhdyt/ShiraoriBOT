9abe42ce821a0097a9c7fdec531712b8e61ee63b
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-09 16:30:57 +0800

    Update elf.js

--- plugins/elf.js
+++ plugins/elf.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/elf?apikey=39f938655e624cb72a79560b', '', '', m)
 }
 handler.help = ['elf']
-handler.tags = ['premium']
+handler.tags = ['anime']
 handler.command = /^(elf)$/i
 
-handler.premium = true
+handler.limit = 2
 
 module.exports = handler
 

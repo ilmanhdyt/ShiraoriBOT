30636bacadf19aa41a0a4a9367a1f074672e5c1f
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-09 13:40:01 +0800

    Update tebakbendera_hint.js

--- plugins/tebakbendera_hint.js
+++ plugins/tebakbendera_hint.js
@@ -6,6 +6,6 @@ let handler = async (m, { conn }) => {
     let clue = json.jawaban.replace(/[AIUEOaiueo]/g, '_')
     conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tebakbendera[id][0])
 }
-handler.command = /^calo$/i
+handler.command = /^tebe$/i
 handler.limit = true
 module.exports = handler

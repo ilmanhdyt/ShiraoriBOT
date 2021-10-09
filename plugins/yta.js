1e2c72ee6dd1359cd94927781fd3fd11e386d105
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-09 19:12:24 +0800

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -26,7 +26,7 @@ handler.private = false
 handler.admin = false
 handler.botAdmin = false
 
-handler.premium = true
+handler.register = false
 handler.fail = null
 handler.exp = 0
 handler.limit = true

16b3ca1a1514e5e9cde23d10154a0c833e82573c
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-11 07:02:17 +0800

    Update megumin.js

--- plugins/megumin.js
+++ plugins/megumin.js
@@ -5,12 +5,12 @@ let handler = async (m, { conn }) => {
   let json = await res.json()
   if (!json.url) throw 'Eror!'
   conn.sendFile(m.chat, json.url, '', '© shiraori', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
-  await conn.sendButtonImg(m.chat, await (await fetch(json.img)).buffer(), Klik Disni, '© SHIRAORI', 'Next', '.megumin', m)
+  await conn.sendButtonLoc(m.chat, await (await fetch(json.img)).buffer(), Klik Disini, '© SHIRAORI', 'Next', '.megumin', m)
 }
 handler.help = ['megumin']
 handler.tags = ['anime']
 handler.command = /^(megumin)$/i
 
 handler.limit = true
-//buatan ftwrr
+//buatan ftwrr(si Fatur)
 module.exports = handler

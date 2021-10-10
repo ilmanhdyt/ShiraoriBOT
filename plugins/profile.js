07180e1c4e0976013ed8ebce898ab6c9c51462e7
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-10 15:11:39 +0800

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -26,9 +26,10 @@ let handler = async (m, { conn, usedPrefix }) => {
         role: 'Warrior V',
         autolevelup: false,
         pc: 0,
+        pasangan: '',
       }
     }
-    let { name, limit, exp, lastclaim, registered, regTime, age, level, role, banned } = global.db.data.users[who]
+    let { name, limit, exp, lastclaim, registered, regTime, age, level, role, pasangan, banned } = global.db.data.users[who]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let math = max - xp

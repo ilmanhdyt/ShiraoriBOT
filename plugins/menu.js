2544c4445527eff1710119d806dd27ac6653d3de
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-11 00:24:26 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -388,7 +388,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send3ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'Ilman\nSHIRAORI BOT MADE BY ILMAN', 'PEMILIK SHIRAORI', '.owner', 'ADMIN SHIRAORI', '.adminshiraori', 'KICKALL', '.kickall', m)
+    await conn.send3ButtonLoc(m.chat, await (await fetch(https://ibb.co/x3Rfy1w + teks)).buffer(), text.trim(), 'Ilman\nSHIRAORI BOT MADE BY ILMAN', 'PEMILIK SHIRAORI', '.owner', 'ADMIN SHIRAORI', '.adminshiraori', 'KICKALL', '.kickall', m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e

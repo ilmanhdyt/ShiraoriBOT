09f10fa31beebfebd3c8958294c69d761f8d32b8
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-11 09:06:53 +0800

    Update snk.js

--- plugins/snk.js
+++ plugins/snk.js
@@ -2,18 +2,18 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-*Bot ini menggunakan open source
-yang dibangun dengan nodejs*
+*Bot ini menggunakan open source*
+*yang dibangun dengan nodejs*
 
-*bot ini menggunakan sc buatan Nurutomo
-dan di tulis ulang/recode*
+*bot ini menggunakan sc buatan Nurutomo*
+*dan di tulis ulang/recode*
 
-https://github.com/Hidayatilman/ShiraoriBOT
+https://github.com/ilmanhdyt/ShiraoriBOT
 
 *Thank To*
-> Nurutomo
-> ariffb
-> BochillGaming
+- Nurutomo
+- ariffb
+
 `.trim(), m)
     let mentionedJid = [m.sender]
 }

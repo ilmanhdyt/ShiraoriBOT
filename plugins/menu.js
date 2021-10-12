edf3ccd70457a1271b7f6c5b33f86c347fb911f0
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-13 07:26:55 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -421,16 +421,16 @@ function ucapan() {
   const time = moment.tz('Asia/Jakarta').format('HH')
   res = "Selamat dinihari"
   if (time >= 4) {
-    res = "Selamat pagi"
+    res = "Selamat pagi🌄"
   }
   if (time > 10) {
-    res = "Selamat siang"
+    res = "Selamat siang🌄"
   }
   if (time >= 15) {
-    res = "Selamat sore"
+    res = "Selamat sore🌅"
   }
   if (time >= 18) {
-    res = "Selamat malam"
+    res = "Selamat malam🌃"
   }
   return res
 }

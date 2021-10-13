2c0a55abde12c5e80ba5024c8b019c29c94f6c61
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-13 17:39:39 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,7 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => await conn.sendFile(m.chat, 'https://telegra.ph/file/ce200b33ebdc9ef61074a.jpg' ", 'tes doang', m, 0, { thumbnail: Buffer.alloc(0) })
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+  conn.sendFile(m.chat, url, 'gimage', '', m, 0, { thumbnail: await (await fetch('https://telegra.ph/file/ce200b33ebdc9ef61074a.jpg')).buffer() })
+}
 ┌〔 Donasi • Emoney 〕
 ├ https://saweria.co/ilmanhdyt
 ├ Pulsa Telkomsel : 081251047727

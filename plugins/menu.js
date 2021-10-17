adf3ffaa7936c65b75f4a050a23f2bcb1135fe40
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-17 16:08:11 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -385,7 +385,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send3ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'Ilman\nSHIRAORI BOT MADE BY ILMAN', 'PEMILIK SHIRAORI', '.owner', 'ADMIN SHIRAORI', '.adminshiraori', 'DONASI', '.donasi', m)
+    await conn.sendButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), `${ucapan} ${name}\nSHIRAORI BOT MADE BY ILMAN`, 'PEMILIK SHIRAORI', '.owner', m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e

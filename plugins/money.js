d4c327899f1851b62865bf6ac1e14c681d24316e
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-19 00:54:00 +0800

    Update money.js

--- plugins/money.js
+++ plugins/money.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-    m.reply(`*${user.money}*`)
+    m.reply(`*${user.money}* Money`)
 }
 handler.help = ['money', 'uang']
 handler.tags = ['rpg']

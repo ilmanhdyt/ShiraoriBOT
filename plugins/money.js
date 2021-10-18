2a35df8d0406b9b3fd96da5c59879a990fbf83d1
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-19 00:51:21 +0800

    Create money.js

---
+++ plugins/money.js
@@ -0,0 +1,11 @@
+let handler = async (m) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+    m.reply(`*${user.money}*`)
+}
+handler.help = ['money', 'uang']
+handler.tags = ['rpg']
+handler.command = /^(money|uang)$/i
+module.exports = handler

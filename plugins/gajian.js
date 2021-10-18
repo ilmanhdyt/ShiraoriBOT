456f0847c86d72f1a3399f7f511214b8896874a4
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-18 23:48:13 +0800

    Update gajian.js

--- plugins/gajian.js
+++ plugins/gajian.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn }) => {
-  let LastClaim = global.DATABASE._data.users[m.sender].lastclaim
+  let LastClaim = global.db.data.users[m.sender].lastclaim
 let cdm = `${MeNit(new Date - LastClaim)}`
 let cds = `${DeTik(new Date - LastClaim)}`
 let cd1 = Math.ceil(44 - cdm)
 let cd2 = Math.ceil(59 - cds)
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 2700000) {
-    global.DATABASE._data.users[m.sender].uang += 7000
-    global.DATABASE._data.users[m.sender].exp += 100
+    global.db.data.users[m.sender].uang += 7000
+    global.db.data.users[m.sender].exp += 100
     m.reply('Nih gaji lu +Rp7000')
-    global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
+    global.db.data.users[m.sender].lastclaim = new Date * 1
   } else throw `Lu udah ambil jatah hari ini.\n\nTunggu ${cd1} Menit ${cd2} Detik!`
 }
 handler.help = ['gaji', 'gajian']

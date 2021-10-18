ed69c9ad1b11225c04de053ebc3fbe928044042f
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-18 08:26:17 +0800

    Create tebakbendera.js

---
+++ plugins/tebakbendera.js
@@ -0,0 +1,41 @@
+let fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 2500
+let handler  = async (m, { conn, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
+        throw false
+    }
+    let res = await fetch(global.API('xteam', '/game/tebakbendera', {}, 'APIKEY'))
+    if (res.status !== 200) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
+    let caption = `
+*「 Tebak Bendera 」*
+
+"${json.bendera} Bendera negara manakah ini?"
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik *${usedPrefix}tbhint* untuk bantuan
+Bonus: ${poin}XP
+    `.trim()
+    conn.tebakbendera[id] = [
+      await conn.reply(m.chat, caption, m),
+      json, poin,
+      setTimeout(() => {
+        if (conn.tebakbendera[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakbendera[id][0])
+        delete conn.tebakbendera[id]
+      }, timeout)
+    ]
+  }
+  handler.help = ['tebakbendera']
+  handler.tags = ['game']
+  handler.command = /^tebakbendera/i
+  handler.register = true
+
+  handler.limit = true
+  
+  module.exports = handler

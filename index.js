1931ad1ec9712a016cedf5a8b673a77b13732ab2
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-18 17:52:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts = require('cfonts')
-CFonts.say('ShiraoriBOT', {
+CFonts.say('SHIRAORI\nBOT', {
   colors: ['#ff33cc'],
   font: 'block',
   align: 'center',

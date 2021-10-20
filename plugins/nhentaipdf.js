21f16df84a367b641f6540a2716e598b855981a9
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-20 08:07:51 +0800

    Create nhentaipdf.js

---
+++ plugins/nhentaipdf.js
@@ -0,0 +1,28 @@
+let { get } = require('axios')
+let handler = async (m, { conn, args }) => {
+   if (!args[0]) throw 'Ehm...kode nuklir nya mana?' 
+  m.reply('Searching....')
+  let lol = `http://api.lolhuman.xyz/api/nhentaipdf/${args[0]}?apikey=BandarBokep`
+//  let lol = global.API('lol', `/api/nhentaipdf/${args[0]}`, {}, 'apikey')
+  let { result } = (await get(lol)).data
+  if (result.includes('HTML')) throw `Code ${args[0]} Not Found!`
+   conn.sendMessage(m.chat, await getBuffer(result), 'documentMessage', { quoted: m, filename: `Tobat Bro Tobat.pdf`, mimetype: 'application/pdf' })
+}
+handler.help = ['nhentai'].map(v => v + ' <kode>')
+handler.tags = ['nsfw']
+handler.command = /^(nhentai)$/i
+handler.premium = true
+
+module.exports = handler
+
+
+
+async function getBuffer(url) {
+
+k = await require('node-fetch')(url)
+
+a = await k.buffer()
+
+return a 
+
+}

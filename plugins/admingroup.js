fd43d0f32e2d6a5f1d6ff20a0245ca8a7eb3d795
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 01:59:29 +0800

    update admingroup.js

--- plugins/admingroup.js
+++ plugins/admingroup.js
@@ -1,2 +1,10 @@
-sjjsks
-sjsjs
+let handler = async (m, { conn, participants, groupMetadata, text }) => {
+  
+  const getGroupAdmins = (participants) => {
+    admins = []
+    for (let i of participants) {
+      i.isAdmin ? admins.push(i.jid) : ''
+    }
+    return admins
+  }
+}

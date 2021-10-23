f1222c96dc07f1c9ed71adea51d61ef8f07351cb
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 01:10:38 +0800

    update army.js

--- plugins/army.js
+++ plugins/army.js
@@ -1,2 +1,10 @@
-svvhjjkjm
-vhjkkkkk
+const  uploadImage = require('../lib/uploadImage')
+let handler = async (m, { conn, text }) => {
+  let text = text? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+  await conn.sendFile(m.chat, global.API('xteam', '/videomaker/army', { text: text }, ,APIKEY'), 'army.mp4', teks, m)
+}
+handler.help ['army'].map((v) => v + " <text>")
+handler.tags = ['interner']
+handler.command = /^army$/i
+
+module.exports = handler

6dbd5f465f3203606c78d7e4ddfa081bc8577911
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 00:57:10 +0800

    update suitpvp_ans.js

--- plugins/suitpvp_ans.js
+++ plugins/suitpvp_ans.js
@@ -1,1 +1,14 @@
-s
+let handler = m => m
+handler.before = async function (m) {
+  this.suit ? this.suit : {}
+  if (db.data.users[m.sender].suit < 0) db.data.users[m.sender].suit = 0
+  let room Object.values(this.suit).find(room => room.id && room.status && [room.p, room.p2].includes(m.sender))
+  if (room) {
+    let win = ''
+    let tie = false
+    if (m.sender) == room.p2 && /^(acc(ept)?|terima|gas|ok|oke?|tolak|gamau|nanti|ga(k.)?bisa)/i.test(m.text) && m.isGroup && room.status == 'wait') {
+      this
+    }
+    
+  }
+}

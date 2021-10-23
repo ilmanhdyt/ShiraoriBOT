76ee7117c871621376cdfe0a3c918e43424e2656
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-23 20:57:07 +0800

    update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(tle + 'donasi')).buffer(), `
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
 ├ https://saweria.co/ilmanhdyt
 ├ Telkomsel:081351047727
 └────
-`.trim(), '© SHIRAORI', 'Donasi', '.donasi', m)
+`.trim(), '© SHIRAORI', 'Pemilik Bot', '.owner', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i

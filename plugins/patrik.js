29d4c70c2a2b0543025c2b1a638bb382db3b469f
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 03:07:34 +0800

    update patrik.js

--- plugins/patrik.js
+++ plugins/patrik.js
@@ -1,2 +1,11 @@
-ssk
-sjskskksks
+let handler = async (m, { conn }) => {
+  await conn.sendFile(m.chat, global.API('lolhum', '/api/sticker/patrick', { text: text }, ,APIKEY'), 'patrick.jpg', kamu gay, m)
+}
+handler.help = ['stikerpatrick']
+handler.tags = ['internet']
+handler.command = /^(stikerpatrick)$/i
+
+handler.limit = true
+handler.group = true
+
+module.exports = handler

a8facf09dab559d496477d64b0b93eb06f86931d
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 02:15:44 +0800

    update admingroup.js

--- plugins/admingroup.js
+++ plugins/admingroup.js
@@ -7,4 +7,21 @@ let handler = async (m, { conn, participants, groupMetadata, text }) => {
     }
     return admins
   }
+  
+  let pp = './src/logo_admin.jpg'
+  try {
+      pp = await conn.getProfilePicture(m.chat)
+  } catch (e) {
+  } finally {
+    const groupAdmins = getGroupAdmins(participants)
+    let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split`@`[0]}`).join('\n')
+*Admin Grup*
+${listAdmin}
+
+let mentionedJid = groupAdmins.contact([`${m.chat.split`-`[0]}@s.whatsapp.net`])
+conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', caption, m, 0, { contextInfo: { mentionedJid } })
+  }
 }
+handler.help = ['admingrup', 'admingc']
+handler.tags = ['group']
+handler.command = /^(admingroup|admingc)$/i

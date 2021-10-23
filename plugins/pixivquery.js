29f8dbe4bc6cf71f59fcc1a1b380748611cc0870
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 02:48:49 +0800

    update pixivquery.js

--- plugins/pixivquery.js
+++ plugins/pixivquery.js
@@ -1,17 +1,16 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `cari apa?\nContoh:\n${usedPrefix + command} Shiraori`
-  let res = await fetch(global.API('lolhum', '/api/pixiv2', {
-    q: text
-  }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (!json.status) throw json
-  let pin = json.data[Math.floor(Math.random() * json.data.lenght)];
-  conn.sendFile(m.chat, pixiv, '', '© SHIRAORI', m, 0, { thumbnail: await (await fetch(pin)).buffer() })
-  }
-  handler.help = ['pixiv <pencarian>']
-  handler.tags = ['internet']
-  handler.command = /^(pixiv)$/i
+const fetch = require('node-fetch')
 
-  module.exports = handler
+let handler = async (m, { conn }) => {
+    let res = await fetch(global.API('lolhum', '/api/pixiv', {}, 'apikey'))
+    if (!res.ok) throw await res.text()
+    let img = await res.buffer()
+    if (!img) throw img
+    conn.sendFile(m.chat, img, '', '© SHIRAORI', m, 0, { thumbnail: await (await fetch(img)).buffer()
+}
+handler.help = ['pixiv <pencarian>']
+handler.tags = ['internet']
+handler.command = /^(pixiv)$/i
+
+handler.limit = true
+
+module.exports = handler

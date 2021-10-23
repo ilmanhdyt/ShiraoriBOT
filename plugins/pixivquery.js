8781d3cfb94e8a5aef4bebf0dfb7a22ce251a8f1
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 03:00:22 +0800

    update pixivquery.js

--- plugins/pixivquery.js
+++ plugins/pixivquery.js
@@ -1,15 +1,15 @@
-const axios = require('axios')
+const fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
-    let res = await fetch(global.API('lolhum', '/api/pixiv2', {}, 'apikey'))
+    let res = await fetch(global.API('lolhum', '/api/konachan', {}, 'apikey'))
     if (!res.ok) throw await res.text()
     let img = await res.buffer()
     if (!img) throw img
     conn.sendFile(m.chat, img, '', '© SHIRAORI', m, 0, { thumbnail: await (await fetch(img)).buffer()
 }
-handler.help = ['pixiv <pencarian>']
+handler.help = ['konachan <pencarian>']
 handler.tags = ['internet']
-handler.command = /^(pixiv)$/i
+handler.command = /^(konachan)$/i
 
 handler.limit = true
 

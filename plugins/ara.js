67070427420e0440498b2bcd4c1d2e8c77ce81c3
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 10:30:19 +0800

    update ara.js

--- plugins/ara.js
+++ plugins/ara.js
@@ -1,2 +1,9 @@
-sl
-djdkdkd
+let fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let ara = fs.readFileSync('./audio/Ara.mp3')
+conn.sendMessage(m.chat, ara, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+} 
+handler.customPrefix = ['ara']
+handler.tags = ['fun']
+handler.command = /^(araara)$/i

10691da60c887c8b0bcc4d587d29c47631cf15fe
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 13:40:49 +0800

    Ini sagiri

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
  
   conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/sagiri?apikey=HIRO', '', 'Sagiri', m)
 }
-handler.help = ['elaina']
+handler.help = ['sagiri']
 handler.tags = ['anime']
-handler.command = /^(elaina)$/i
+handler.command = /^(sagiri)$/i
 
 handler.limit = true
 handler.group = true

27d1420429ea1d35b77cd71858e7b219fdb17642
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 12:33:33 +0800

    update elaina.js

--- plugins/elaina.js
+++ plugins/elaina.js
@@ -5,4 +5,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 let hasil = await getBuffer('https://lolhuman.xyz/api/random/elaina?apikey=HIRO')
 
 await conn.sendButtonImg(m.chat, await fetch(Img)).buffer(),`
+`.trim(), 'Elaina Punya Ilman', 'Next', '.elaina')
 }
+handler.help = ['elaina']
+handler.tags = ['anime']
+handler.command /^(elaina)$/i

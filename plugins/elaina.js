c14607ccbd63d95c33ff11bf7b12f84658ece655
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 12:27:24 +0800

    update elaina.js

--- plugins/elaina.js
+++ plugins/elaina.js
@@ -1,3 +1,8 @@
-sjskk
+let { getBuffer } = require('../lib/function')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
 
-ddkdkd
+let hasil = await getBuffer('https://lolhuman.xyz/api/random/elaina?apikey=HIRO')
+
+await conn.sendButtonImg(m.chat, await fetch(Img)).buffer(),`
+}

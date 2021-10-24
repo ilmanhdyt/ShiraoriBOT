d50e34718d3fa9d984ac370f30a7996f6dd7b14a
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 10:34:08 +0800

    update ara.js

--- plugins/ara.js
+++ plugins/ara.js
@@ -4,6 +4,7 @@ let handler = async (m) => {
 let ara = fs.readFileSync('./audio/Ara.mp3')
 conn.sendMessage(m.chat, ara, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 } 
-handler.customPrefix = ['ara']
-handler.tags = ['fun']
-handler.command = /^(araara)$/i
+handler.customPrefix [ /^ara ?ara$/i
+handler.command = new RegExp
+
+module.exports = handler

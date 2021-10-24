2c0531ecc50dbac9ac3881824433fbfc9b90cf43
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-24 12:56:17 +0800

    update elaina.js

--- plugins/elaina.js
+++ plugins/elaina.js
@@ -1,7 +1,7 @@
 let fetch = require ('node-fetch')
 let res = await fetch(global.API('lolhum', '/api/random/elaina', {
 }, 'apikey'))
-await conn.sendButtonImg(m.chat, await (await fetch(res)).buffer(), "Elaina Punya Ilman", '© SHIRAORI BOT', 'NEXT', `.elaina`, m)
+await conn.sendButtonImg(m.chat, await (await fetch(result)).buffer(), "Elaina Punya Ilman", '© SHIRAORI BOT', 'NEXT', `.elaina`, m)
 }
 handler.help = ['elaina']
 handler.tags = ['anime']

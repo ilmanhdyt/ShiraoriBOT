9bcbcf826cbfac51ca8a28f304ff4ce5526380e4
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-26 09:54:41 +0800

    update snk.js

--- plugins/snk.js
+++ plugins/snk.js
@@ -1,3 +1,6 @@
+//jangan di hapus yee kontol tambahin aja
+// lu tu cuman recode
+
 let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
@@ -5,15 +8,15 @@ let handler = async (m, { conn, text }) => {
 *Bot ini merupakan Bot WhatsApp yang dibangun dengan menggunakan nodejs*
 
 *Yang dibuat oleh Nurutomo*
-
+*Dan ditulis ulang/recode oleh Ariffb*
 
 *ingin membuat bot?* 
 *bot ini open resource loh*
 *bisa kalian coba dengan menekan link dibawah ini*
-=> *https://github.com/ilmanhdyt/ShiraoriBOT*
+*https://github.com/ilmanhdyt/ShiraoriBOT*
 
 *Thanks To*
-=> *Nurutomo > https://github.com/Nurutomo
+=> *Nurutomo* > https://github.com/Nurutomo
 => *Ariffb* > https://github.com/ariffb25
 => *BochilGaming* > https://github.com/BochilGaming
 => *ilmanhdyt* > https://github.com/ilmanhdyt

a9b0d2beefc0c49e3830b415218ab59aa24056f5
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-26 20:56:30 +0800

    update yaoi.js

--- plugins/yaoi.js
+++ plugins/yaoi.js
@@ -1,2 +1,13 @@
-sjsnms
-sksks
+
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, '', '', 'sange~an', m)
+}
+handler.help = ['yaoi']
+handler.tags = ['nsfw']
+handler.command = /^(yaoi)$/i
+
+handler.limit = true
+handler.group = true
+handler.nsfw = true
+
+module.exports = handler

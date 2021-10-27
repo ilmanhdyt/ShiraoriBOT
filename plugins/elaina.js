a14cd38da544ea575df9ca6a2e1358c4cba097b5
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-28 07:25:04 +0800

    update elaina.js

--- plugins/elaina.js
+++ plugins/elaina.js
@@ -1,24 +1,11 @@
-var {WAMessageProto} = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, text }) => {
-heum = await require('node-fetch')('https://api.lolhuman/api/random/elaina?apikey=HIRO'').then(v => v.json())
-let url = heum[Math.floor(Math.random() * heum.length)]
- let buttons = [
-  {buttonId: '.elaina', buttonText: {displayText: 'Get Again'}, type: 1}
-]
-const buttonsMessage = {
-    contentText: `
-Elaina
-`.trim(),    footerText: '© Elaina Punya ilman',
-    buttons: buttons,
-  imageMessage: await conn.toMSG({ url }, 'imageMessage'),
-    headerType: 'IMAGE'
+let handler = async (m, { conn }) => {
+  await conn.sendFile(m.chat, global.API('lolhum', '/random/elaina', { text: © SHIRAORI }, ,APIKEY'), 'elaina.jpg', teks, m)
 }
-const sendMsg = await conn.prepareMessageFromContent(m.chat,{buttonsMessage},{ quoted: m})
-
-conn.relayWAMessage(sendMsg)
-}
-handler.command = /^(elaina)$/i
-handler.tags = ['random']
 handler.help = ['elaina']
+handler.tags = ['random']
+handler.command = /^(elaina)$/i
+
+handler.limit = true
+handler.group = true
+
 module.exports = handler

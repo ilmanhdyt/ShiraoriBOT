df18f50b4a9be702bc124020779b458261f571ea
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 18:51:33 +0800

    update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,16 +1,16 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 
-if (!args[0]) throw 'usernamenya mana om'
-if (!args[1]) throw 'repo nya mana?'
+if (!args[0]) throw 'usernamenya mana om\nContoh ilmanhdyt'
+if (!args[1]) throw 'repo nya mana?\nContoh ShiraoriBOT'
 
 let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
 //ByRizkyAdi
-m.reply(`sedang mengungkapkan file ke zip`)
+m.reply(`waiting for compress to zip`)
 conn.sendFile( m.chat, url, 'repo.zip', null, m)
 
 }
 handler.help = ['githubdl']
-handler.tags = ['downloader']
+handler.tags = ['github']
 handler.command = /githubdl/i
 
 handler.limit = 5

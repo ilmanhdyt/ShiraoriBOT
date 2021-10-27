a8c5fe818641b76c703ede4f0dc858a9b8f6524c
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 11:53:45 +0800

    update blowjob.js

--- plugins/blowjob.js
+++ plugins/blowjob.js
@@ -1,11 +1,10 @@
 let handler = async (m, { conn }) => {
- 
-  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/blowjob?APIKEY=HIRO', '', 'sange~an', m)
+ await conn.sendFile(m.chat, global.API('xteam', '/randomimage/blowjob', { text: © SHIRAORI }, ,APIKEY'), 'blowjob.jpg', teks, m)
   
 }
-handler.help = ['cum']
+handler.help = ['blowjob']
 handler.tags = ['nsfw']
-handler.command = /^(cum)$/i
+handler.command = /^(blowjob)$/i
 
 handler.limit = true
 handler.group = true

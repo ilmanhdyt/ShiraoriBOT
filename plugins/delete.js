19e505e4ec452cb004d3229ae9d9a8e428150ae9
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 09:23:58 +0800

    update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -2,7 +2,7 @@ let handler = function (m) {
   if (!m.quoted) throw false
   let { chat, fromMe, id, isBaileys } = m.quoted
   if (!fromMe) throw false
-  if (/Stikerin Broadcast/i.test(m.quoted.text)) throw 'Tidak bisa menghapus pesan broadcast!'
+  if (/Shiraori Broadcast/i.test(m.quoted.text)) throw 'Tidak bisa menghapus pesan broadcast!'
   if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
   this.deleteMessage(chat, {
     fromMe,
@@ -14,5 +14,6 @@ handler.help = ['del', 'delete']
 handler.tags = ['info']
 
 handler.command = /^del(ete)?$/i
+handler.limit = 2
 
 module.exports = handler

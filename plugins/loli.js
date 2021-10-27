a08dc94becfeb675f87e086ebf0ea78a01b239d9
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-28 07:18:07 +0800

    update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,12 +1,24 @@
-let handler = async (m, { conn }) => {
- 
-  conn.sendFile(m.chat, 'https://hardianto-chan.herokuapp.com/api/anime/loli?apikey=hardianto', '', 'dasar om om', m)
+var {WAMessageProto} = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+heum = await require('node-fetch')('https://raw.githubusercontent.com/Xmell91/loli/master/loli.json').then(v => v.json())
+let url = heum[Math.floor(Math.random() * heum.length)]
+ let buttons = [
+  {buttonId: '/loli', buttonText: {displayText: 'Get Again'}, type: 1}
+]
+const buttonsMessage = {
+    contentText: `
+Random Loli
+`.trim(),    footerText: '© huii lolicon',
+    buttons: buttons,
+  imageMessage: await conn.toMSG({ url }, 'imageMessage'),
+    headerType: 'IMAGE'
 }
-handler.help = ['loli']
-handler.tags = ['anime']
+const sendMsg = await conn.prepareMessageFromContent(m.chat,{buttonsMessage},{ quoted: m})
+
+conn.relayWAMessage(sendMsg)
+}
 handler.command = /^(loli)$/i
-
-handler.limit = true
-handler.group = true
-
+handler.tags = ['random']
+handler.help = ['loli']
 module.exports = handler

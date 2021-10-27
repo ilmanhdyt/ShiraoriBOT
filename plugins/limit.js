2e7f9f89259f4310cdc5ef1339a0ce986706da20
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 13:45:15 +0800

    update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-    m.reply(`*${user.limit}* Limit\n*${user.exp}* XP\nUang *${user.money}*\nLevel *${user.level}*\nRole *${user.role}*\nPasangan ${global.db.data.users[pasangan].registered ? "(" + global.db.data.users[pasangan].name + ") (@${pasangan.replace( /@.+/, "" )})` : "Jomblo"}`)
+    m.reply(`*${user.limit}* Limit\n*${user.exp}* XP\nUang *${user.money}*\nLevel *${user.level}*\nRole *${user.role}*\nPasangan ${global.db.data.users[pasangan].registered ? "(" + global.db.data.users[pasangan].name + ") (@${pasangan.replace( /@.+/, "" )})` : "Jomblo"}
 }
 handler.help = ['my [@user]']
 handler.tags = ['xp']

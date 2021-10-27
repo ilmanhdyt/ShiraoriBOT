eba07880b843fdaefcc56bbf8f2915fd5ef66348
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 14:06:03 +0800

    update money.js

--- plugins/money.js
+++ plugins/money.js
@@ -1,7 +1,6 @@
 let PhoneNumber = require('awesome-phonenumber')
 let levelling = require('../lib/levelling')
 let handler = async (m, { conn, usedPrefix }) => {
-  let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
     pp = await conn.getProfilePicture(who)

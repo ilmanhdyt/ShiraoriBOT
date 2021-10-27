c276c248cd36b3997afb02f3a0a2ff24a635ceab
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 10:33:00 +0800

    update anime_fanart.js

--- plugins/anime_fanart.js
+++ plugins/anime_fanart.js
@@ -1,1 +1,12 @@
-sks
+
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, 'https://api.lolhuman/api/random/art?apikey=HIRO', '', 'ShiraoriBOT', m)
+}
+handler.help = ['fanart', 'animefanart']
+handler.tags = ['anime', 'random']
+handler.command = /^(fanart|animefanart)$/i
+
+handler.limit = 5
+handler.group = true
+
+module.exports = handler

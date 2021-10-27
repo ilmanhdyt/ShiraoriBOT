ea86e7dba9ed75da4b746c9f89cb6a04b8821f1e
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 11:51:07 +0800

    update yuri.js

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -1,10 +1,9 @@
 let handler = async (m, { conn }) => {
-
-  conn.sendFile(m.chat, 'https://bx-hunter.herokuapp.com/api/nsfw/yuri?apikey=Ikyy69', '', 'sange~an', m)
+ await conn.sendFile(m.chat, global.API('bx', '/nsfw/yuri', { text: © SHIRAORI }, ,APIKEY'), 'yuri.jpg', teks, m)
 }
-handler.help = ['random yuri]
+handler.help = ['yuri2'']
 handler.tags = ['nsfw']
-handler.command = /^(yuri)$/i
+handler.command = /^(yuri2)$/i
 
 handler.limit = true
 handler.group = true

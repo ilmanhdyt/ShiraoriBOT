6e8bba25360c4883a705dd1a7aa1b929cfce6b40
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-28 06:58:57 +0800

    update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -1,23 +1,36 @@
-let fs = require('fs')
-let handler = async (m, { conn, args }) => {
-
-    const json = JSON.parse(fs.readFileSync('./src/premium.json'))
+let handler = async (m, { text, usedPrefix, command }) => {
     let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
-    else who = args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if (json.includes(who.split`@`[0])) throw `${conn.getName(who)} sudah premium!`
-    json.push(`${who.split`@`[0]}`)
-    fs.writeFileSync('./src/premium.json', JSON.stringify(json))
-    m.reply(`${conn.getName(who)} sekarang premium!`)
-
-    delete require.cache[require.resolve('../config')]
-    require('../config')
-
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = db.data.users[who]
+    if (!who) throw `tag or mention someone!`
+    let txt = text.replace('@' + who.split`@`[0], '').trim()
+    if (!txt) throw `where the number of days?`
+    if (isNaN(txt)) return m.reply(`only number!\n\nexample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+    var jumlahHari = 86400000 * txt
+    var now = new Date() * 1
+    if (now < user.premiumTime) user.premiumTime += jumlahHari
+    else user.premiumTime = now + jumlahHari
+    user.premium = true
+    m.reply(`successfully added *${user.name}* to be a premium user for ${txt} days.\n\ncountdown: ${msToDate(user.premiumTime - now)}`)
 }
-handler.help = ['addprem [@user]']
+handler.help = ['addprem [@user] <amount of days>']
 handler.tags = ['owner']
 handler.command = /^(add|tambah|\+)prem$/i
 
-handler.owner = true
+handler.rowner = true
 
 module.exports = handler
+
+function msToDate(ms) {
+    temp = ms
+    days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    daysms = ms % (24 * 60 * 60 * 1000);
+    hours = Math.floor((daysms) / (60 * 60 * 1000));
+    hoursms = ms % (60 * 60 * 1000);
+    minutes = Math.floor((hoursms) / (60 * 1000));
+    minutesms = ms % (60 * 1000);
+    sec = Math.floor((minutesms) / (1000));
+    return days + " days " + hours + " hours " + minutes + " minutes";
+    // +minutes+":"+sec;
+}

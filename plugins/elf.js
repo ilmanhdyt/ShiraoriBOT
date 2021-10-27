954320cfd9aa70550c98d8c1080670cc4aa49851
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 11:32:12 +0800

    update elf.js

--- plugins/elf.js
+++ plugins/elf.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  await conn.sendFile(m.chat, global.API('lolhum', '/random/elf', { text: text }, ,APIKEY'), 'elf.jpg', teks, m)
+  await conn.sendFile(m.chat, global.API('lolhum', '/random/elf', { text: text }, ,APIKEY'), 'elf.jpg', caption, m)
 }
 handler.help = ['elf']
 handler.tags = ['random']

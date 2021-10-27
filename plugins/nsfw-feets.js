c28c2953a815f0e9daa7f0ec88e39883ecc4ff81
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 10:36:22 +0800

    update nsfw-feets.js

--- plugins/nsfw-feets.js
+++ plugins/nsfw-feets.js
@@ -1,1 +1,13 @@
-zz
+
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, 'https://api.lolhuman/api/random/nsfw/animefeets?apikey=HIRO', '', 'jadi sange', m)
+}
+handler.help = ['feets']
+handler.tags = ['nsfw']
+handler.command = /^(feets)$/i
+
+handler.limit = true
+handler.group = true
+handler.nsfw = true
+
+module.exports = handler

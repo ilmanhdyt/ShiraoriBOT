0e7e3f4f183a64a008aadc65a7745e7330306213
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 10:23:00 +0800

    update shinobu.js

--- plugins/shinobu.js
+++ plugins/shinobu.js
@@ -1,1 +1,11 @@
-https://api.lolhuman.xyz/api/random/shinobu?apikey=HIRO
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/shinobu?apikey=HIRO', '', '@6281351047727', m)
+}
+handler.help = ['shinobu']
+handler.tags = ['random']
+handler.command = /^(shinobu)$/i
+
+handler.limit = 2
+handler.group = true
+
+module.exports = handler

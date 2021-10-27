7e19639d956f267be95805d7d3a797ba9b4827c2
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-27 10:28:09 +0800

    update kanna.js

--- plugins/kanna.js
+++ plugins/kanna.js
@@ -1,1 +1,12 @@
-ss
+
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, 'https://api.lolhuman/api/random/kanna?apikey=HIRO', '', 'miss kobayashi dragon maid', m)
+}
+handler.help = ['kanna']
+handler.tags = ['random']
+handler.command = /^(kanna)$/i
+
+handler.limit = true
+handler.group = true
+
+module.exports = handler

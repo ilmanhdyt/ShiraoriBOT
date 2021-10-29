b2cfd2de424bdca17eb2cd0de27243be1b4ddaaf
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-29 19:51:18 +0800

    Create buymoney.js

---
+++ plugins/buymoney.js
@@ -0,0 +1,28 @@
+const xppermoney = 500
+let handler = async (m, { conn, command, args }) => {
+  let count = command.replace(/^buy/i, '')
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xppermoney) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+  count = Math.max(1, count)
+  if (isNaN(count)) throw `hanya angka!\n\ncontoh: .buymoney5`
+  if (global.db.data.users[m.sender].exp >= xppermoney * count) {
+    global.db.data.users[m.sender].exp -= xppermoney * count
+    global.db.data.users[m.sender].money += count
+    conn.reply(m.chat, `-${xppermoney * count} XP\n+ ${count} Money`, m)
+  } else conn.reply(m.chat, `XP tidak mencukupi untuk membeli ${count} money`, m)
+}
+handler.help = ['buymoney<jumlah limit>', 'buymoney <jumlah limit>', 'buymoneyall']
+handler.tags = ['rpg']
+handler.command = /^buymoney([0-9]+)|buymoney|buymoneyall$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler

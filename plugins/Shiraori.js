2b5541f03633a9d4a2ac482900038b82e5742db3
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-29 21:59:30 +0800

    update Shiraori.js

--- plugins/Shiraori.js
+++ plugins/Shiraori.js
@@ -1,1 +1,19 @@
-sksk
+let handler = async (m, { conn, text }) => {
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+
+  conn.reply(m.chat,`${pickRandom(global.shiraori)}`, m)
+    let mentionedJid = [m.sender]
+}
+handler.customPrefix = /ilman|Ilman/i
+handler.command = new RegExp
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.shiraori = [
+'halo ilman',
+'knp ilman?',
+]

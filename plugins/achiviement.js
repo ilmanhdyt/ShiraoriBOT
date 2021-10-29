49bccbf4b94521130a313cb5667d9b1c9e007396
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-29 22:43:31 +0800

    update achiviement.js

--- plugins/achiviement.js
+++ plugins/achiviement.js
@@ -1,2 +1,65 @@
-sjsjsjsjsksksk
-kskskskksks
+let levelling = require('../lib/levelling')
+let handler = async (m, { conn, usedPrefix }) => {
+    let healt = global.db.data.users[m.sender].healt
+    let armor = global.db.data.users[m.sender].armor 
+    let warn = global.db.data.users[m.sender].warn
+    let pet = global.db.data.users[m.sender].pet
+    let kucing = global.db.data.users[m.sender].kucing
+    let _kucing = global.db.data.users[m.sender].anakkucing
+    let rubah = global.db.data.users[m.sender].rubah
+    let _rubah = global.db.data.users[m.sender].anakrubah
+    let kuda = global.db.data.users[m.sender].kuda
+    let _kuda = global.db.data.users[m.sender].anakkuda
+    let diamond = global.db.data.users[m.sender].diamond
+    let potion = global.db.data.users[m.sender].potion
+    let common = global.db.data.users[m.sender].common
+    let makananpet = global.db.data.users[m.sender].makananpet
+    let uncommon = global.db.data.users[m.sender].uncommon
+    let mythic = global.db.data.users[m.sender].mythic
+    let legendary = global.db.data.users[m.sender].legendary
+    let level = global.db.data.users[m.sender].level
+    let money = global.db.data.users[m.sender].money
+    let exp = global.db.data.users[m.sender].exp
+    let sampah = global.db.data.users[m.sender].sampah
+    let { max } = levelling.xpRange(level, exp, global.multiplier)
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+    let sortedmoney = Object.entries(global.db.data.users).sort((a, b) => b[1].money - a[1].money)
+    let sortedlevel = Object.entries(global.db.data.users).sort((a, b) => b[1].level - a[1].level)
+    let sorteddiamond = Object.entries(global.db.data.users).sort((a, b) => b[1].diamond - a[1].diamond)
+    let sortedpotion = Object.entries(global.db.data.users).sort((a, b) => b[1].potion - a[1].potion)
+    let sortedsampah = Object.entries(global.db.data.users).sort((a, b) => b[1].sampah - a[1].sampah)
+    let sortedcommon = Object.entries(global.db.data.users).sort((a, b) => b[1].common - a[1].common)
+    let sorteduncommon = Object.entries(global.db.data.users).sort((a, b) => b[1].uncommon - a[1].uncommon)
+    let sortedmythic = Object.entries(global.db.data.users).sort((a, b) => b[1].mythic - a[1].mythic)
+    let sortedlegendary = Object.entries(global.db.data.users).sort((a, b) => b[1].legendary - a[1].legendary)
+    let usersmoney = sortedmoney.map(v => v[0])
+    let usersdiamond = sorteddiamond.map(v => v[0])
+    let userspotion = sortedpotion.map(v => v[0])
+    let userssampah = sortedsampah.map(v => v[0])
+    let userslevel = sortedlevel.map(v => v[0])
+    let userscommon = sortedcommon.map(v => v[0])
+    let usersuncommon = sorteduncommon.map(v => v[0])
+    let usersmythic = sortedmythic.map(v => v[0])
+    let userslegendary = sortedlegendary.map(v => v[0])
+*achievement*
+1.Top level *${userslevel.indexOf(m.sender) + 1}* dari *${userslevel.length}*
+2.Top Money *${usersmoney.indexOf(m.sender) + 1}* dari *${usersmoney.length}*
+3.Top Diamond *${usersdiamond.indexOf(m.sender) + 1}* dari *${usersdiamond.length}*
+4.Top Potion *${userspotion.indexOf(m.sender) + 1}* dari *${userspotion.length}*
+5.Top Common *${userscommon.indexOf(m.sender) + 1}* dari *${userscommon.length}*
+6.Top Uncommon *${usersuncommon.indexOf(m.sender) + 1}* dari *${usersuncommon.length}*
+7.Top Mythic *${usersmythic.indexOf(m.sender) + 1}* dari *${usersmythic.length}*
+8.Top Legendary *${userslegendary.indexOf(m.sender) + 1}* dari *${userslegendary.length}*
+9.Top Sampah *${userssampah.indexOf(m.sender) + 1}* dari *${userssampah.length}*
+\n${readMore}\n
+`.trim()
+    conn.reply(m.chat, str, m)
+}
+handler.help = ['achievement']
+handler.tags = ['rpg']
+handler.command = /^(achievement$/i
+handler.group = true
+module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)

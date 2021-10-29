b9e54b6953de869ef5a43771910f0ee1dedeb29b
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-29 21:40:37 +0800

    update Rido.js

--- plugins/Rido.js
+++ plugins/Rido.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-@6281351047727 ada yg manggil
+@6281351047727 Onii-chan ada yg manggil
 `.trim(), m)
     let mentionedJid = [m.sender]
 }

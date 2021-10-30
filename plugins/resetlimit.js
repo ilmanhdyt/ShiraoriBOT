1ae78ddebcc31da0491b1fa05bb8e990360f114c
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-31 02:31:52 +0800

    update resetlimit.js

--- plugins/resetlimit.js
+++ plugins/resetlimit.js
@@ -1,7 +1,7 @@
 let { Presence } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
 	await conn.updatePresence(m.chat, Presence.composing) 
-	let list = Object.entries(global.DATABASE.data.users)
+	let list = Object.entries(global.db.data.users)
 	let lim = !args || !args[0] ? 100 : isNumber(args[0]) ? parseInt(args[0]) : 100
 	lim = Math.max(1, lim)
 	list.map(([user, data], i) => (Number(data.limit = lim)))

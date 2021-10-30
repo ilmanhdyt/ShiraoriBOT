50cd81c1f751b1f0ce04f8c9cbdc1cb6a97f076f
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-10-30 22:06:56 +0800

    update handler.js

--- handler.js
+++ handler.js
@@ -347,7 +347,7 @@ module.exports = {
           if (xp > 200) m.reply('Ngecit -_-') // Hehehe
           else m.exp += xp
           if (!isPrems && plugin.limit && global.db.data.users[m.sender].limit < plugin.limit * 1) {
-            this.reply(m.chat, `Limit kamu habis, silahkan beli melalui *${usedPrefix}buy*`, m)
+            this.reply(m.chat, `Limit kamu tidak cukup atau habis, silahkan beli melalui *${usedPrefix}buy*`, m)
             continue // Limit habis
           }
           if (plugin.level > _user.level) {

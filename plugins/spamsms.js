c9cd77ffb1d98b304cb5ad82bbf3326f957de837
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-11-01 16:32:56 +0800

    update spamsms.js

--- plugins/spamsms.js
+++ plugins/spamsms.js
@@ -1,7 +1,7 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
 
-    if (!text) return conn.reply(m.chat, 'Masukan No Telpon yang akan di SpamSms!\n\nMisal : !spamsms 6285xxx', m)
+    if (!text) return conn.reply(m.chat, 'Masukan No Telpon yang akan di SpamSms!\n\nMisal : .spamsms 6285xxx', m)
 
 	axios.get(`https://arugaz.herokuapp.com/api/spamsms?no=${text}&jum=20`).then ((res) => {
 	 	let hasil = `${res.data.logs}`
@@ -9,7 +9,7 @@ let handler = async(m, { conn, text }) => {
     conn.reply(m.chat, hasil, m)
 	})
 }
-handler.help = ['spamsms'].map(v => v + ' <no hp>')
+handler.help = ['spamsms'].map(v => v + ' no hp')
 handler.tags = ['premium']
 handler.command = /^(spamsms)$/i
 handler.owner = false

3e5691109b3aca7869250a767f2960f9d485dd77
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-11-01 15:45:58 +0800

    update Rido.js

--- plugins/Rido.js
+++ plugins/Rido.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
- gk boleh ngomong kasar
+ 
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /Ajg|Anj/i
+handler.customPrefix = //i
 handler.command = new RegExp
 
 module.exports = handler

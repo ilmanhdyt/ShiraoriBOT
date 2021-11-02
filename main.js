732717c9bb1f4c1991ff8e958866839f5593805d
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-11-02 13:50:26 +0800

    Engror

--- main.js
+++ main.js
@@ -52,6 +52,8 @@ if (!opts['test']) setInterval(async () => {
 }, 60 * 1000) // Save every minute
 if (opts['server']) require('./server')(global.conn, PORT)
 
+conn.version = [2, 2413, 3]
+conn.connectOptions.maxQueryResponseTime = 60_000
 if (opts['test']) {
   conn.user = {
     jid: '2219191@s.whatsapp.net',

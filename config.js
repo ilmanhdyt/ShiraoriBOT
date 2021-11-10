b006d63e279374d51296fd95ef593afd41890770
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-11-11 01:08:58 +0800

    update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner  = ['6281351047727', '6281351047727'] // letakkan nomor kalian disini
+global.owner  = ['6281351047727', '380942618992'] // letakkan nomor kalian disini
 global.mods  = JSON.parse(fs.readFileSync('./src/moderator.json'))
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -22,20 +22,20 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
-  'https://api.lolhuman.xyz': 'apikeymu',
+  'https://api.xteam.xyz': 'c81b3345e477a0c7',
+  'https://api.lolhuman.xyz': '3af90a5d6317457b1f93e921',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://zekai-api.herokuapp.com': 'yntkts',
-  'https://api.zeks.xyz': 'apifaiz',
+  'https://zekai-api.herokuapp.com': 'tzjHtUpG',
+  'https://api.zeks.xyz': 'apivinz',
 }
 
 // Sticker WM
-global.packname = 'SHIRAORI️ BOT'
+global.packname = 'SHIRAORI️BOT'
 global.author = 'ilman'
 
 global.wait = '_*tunggu..*_'
 global.eror = '*_Server Eror_*'
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.tle = 'https://telegra.ph/file/ecb8f7d2c12d075fc4729.jpg',
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level

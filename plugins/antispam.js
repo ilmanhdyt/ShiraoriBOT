804a899abaa7effd5aa7f5267c763f3c96181866
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-07 09:45:25 +0800

    Biar gk meresahkan

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -5,16 +5,16 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 		await conn.updatePresence(m.chat, Presence.composing) 
 		conn.reply(m.chat, `Contoh :\n	○ ${usedPrefix + command} on\n	○ ${usedPrefix + command} off`, m)
 	} else if(args[0] == 'on') {
-		let cek = global.DATABASE._data.chats[m.chat].antiSpam
+		let cek = global.db.data.chats[m.chat].antispam
 	  if(cek) return conn.reply(m.chat, `*Anti Spam sudah diaktifkan*`, m)
 		await conn.updatePresence(m.chat, Presence.composing) 
-		global.db.data.chats[m.chat].antiSpam = true
+		global.db.data.chats[m.chat].antispam = true
 		conn.reply(m.chat, `*Anti Spam berhasil diaktifkan*\n\n*Bot akan menutup grup otomatis jika mendeteksi spam*`, m)
 	} else if(args[0] == 'off') {
-		let cek = global.db.data.chats[m.chat].antiSpam
+		let cek = global.db.data.chats[m.chat].antispam
 	  if(!cek) return conn.reply(m.chat, `*Anti Spam sudah mati*`, m)
 		await conn.updatePresence(m.chat, Presence.composing) 
-		global.db.data.chats[m.chat].antiSpam = false
+		global.db.data.chats[m.chat].antispam = false
 		conn.reply(m.chat, `*Anti Spam berhasil dinonaktifkan*`, m)
 	} else {
 		await conn.updatePresence(m.chat, Presence.composing) 
